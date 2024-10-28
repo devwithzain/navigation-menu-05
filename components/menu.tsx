5c0e3d892d7ed261b132169ab7a7137a69ee5f68
devwithzain <ZainSoftwear11@gmail.com>
2024-10-29 00:57:58 +0500

    modified

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,6 +1,8 @@
 "use client";
 import gsap from "gsap";
 import Link from "next/link";
+import { motion } from "framer-motion";
+import { navItems, socialItems } from "@/constants";
 import { useEffect, useState } from "react";
 import { CustomEase } from "gsap/CustomEase";
 
@@ -10,32 +12,15 @@ export default function Menu() {
 	const [isAnimating, setIsAnimating] = useState(false);
 	const [menuOpen, setMenuOpen] = useState(false);
 
-	const splitTextIntoSpans = (selector: string) => {
-		const elements = document.querySelectorAll<HTMLElement>(selector);
-		elements.forEach((element) => {
-			const text = element.innerText;
-			const splitText = text
-				.split("")
-				.map(function (char) {
-					return `<span>${char === " " ? "&nbsp;&nbsp;" : char}</span>`;
-				})
-				.join("");
-			element.innerHTML = splitText;
-		});
-	};
-
+	CustomEase.create(
+		"hop",
+		"M0,0 C0.354,0 0.464,0.133 0.498,0.502 0.532,0.872 0.651,1 1,1",
+	);
 	useEffect(() => {
-		CustomEase.create(
-			"hop",
-			"M0,0 C0.354,0 0.464,0.133 0.498,0.502 0.532,0.872 0.651,1 1,1",
-		);
-
-		splitTextIntoSpans(".header h1");
-
-		const menuToggle = document.querySelector(".menu-toggle");
-		const menu = document.querySelector(".menu");
-		const links = document.querySelectorAll<HTMLElement>(".link");
-		const socialLinks = document.querySelectorAll<HTMLElement>(".socials p");
+		const menuToggle = document.querySelector("#menu-toggle");
+		const menu = document.querySelector("#menu");
+		const links = document.querySelectorAll<HTMLElement>("#link");
+		const socialLinks = document.querySelectorAll<HTMLElement>("#socials p");
 
 		const openMenu = () => {
 			setIsAnimating(true);
@@ -75,23 +60,6 @@ export default function Menu() {
 				duration: 1.5,
 				delay: 0.5,
 			});
-
-			gsap.to(".header h1 span", {
-				rotateY: 0,
-				stagger: 0.05,
-				delay: 0.75,
-				duration: 1.5,
-				ease: "power4.out",
-			});
-
-			gsap.to(".header h1 span", {
-				y: 0,
-				scale: 1,
-				stagger: 0.05,
-				delay: 0.5,
-				duration: 1.5,
-				ease: "power4.out",
-			});
 		};
 
 		const closeMenu = () => {
@@ -109,11 +77,6 @@ export default function Menu() {
 					gsap.set(socialLinks, { y: 30, opacity: 0 });
 					gsap.set(".video-wrapper", {
 						clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
-					});
-					gsap.set(".header h1 span", {
-						y: 500,
-						rotateY: 90,
-						scale: 0.75,
 					});
 					setIsAnimating(false);
 				},
@@ -140,46 +103,60 @@ export default function Menu() {
 
 	return (
 		<div className="w-full h-screen bg-[url('/hero.jpg')] bg-cover bg-center">
-			<div className="logo">
-				<Link href="/">Avaro</Link>
-			</div>
-			<div className={`menu-toggle ${menuOpen ? "opened" : "closed"}`}>
-				<div className="menu-toggle-icon">
-					<div className="hamburger">
-						<div
-							className="menu-bar"
-							data-position="top"></div>
-						<div
-							className="menu-bar"
-							data-position="bottom"></div>
+			<div className="w-full flex justify-between items-center px-8 py-4 fixed top-0 left-0 z-50">
+				<Link
+					href="/"
+					className={`uppercase text-[60px] xm:text-[40px] sm:text-[40px] font-light transition-all duration-300 ease-in-out delay-1000 ${
+						menuOpen && "text-white "
+					}`}>
+					avaro
+				</Link>
+				<div
+					className={`menu-toggle fixed top-8 right-8 sm:top-4 xm:top-4 sm:right-4 xm:right-4 sm:h-[50px] xm:h-[50px] w-[120px] h-[60px] bg-[#0f0f0f] rounded-full origin-right cursor-pointer z-20 ease-[0.075, 0.82, 0.165, 1] ${
+						menuOpen ? "opened" : "closed"
+					}`}
+					id="menu-toggle">
+					<div className="menu-toggle-icon sm:h-[50px] xm:h-[50px]">
+						<div className="hamburger">
+							<div
+								className="menu-bar"
+								data-position="top"></div>
+							<div
+								className="menu-bar"
+								data-position="bottom"></div>
+						</div>
 					</div>
-				</div>
-				<div className="menu-copy">
-					<p>Menu</p>
+					<div className="menu-copy">
+						<p className="uppercase text-white text-[60px] font-light">Menu</p>
+					</div>
 				</div>
 			</div>
 
-			<div className="menu">
-				<div className="col col-1">
-					<div className="menu-logo">
-						<Link href="/">Avaro</Link>
+			<div
+				className="menu"
+				id="menu">
+				<div className="relative h-full flex flex-col items-end justify-between pt-32 px-8 pb-8 flex-1 sm:items-start xm:items-start">
+					<div>
+						{navItems.map((item) => (
+							<div
+								key={item.id}
+								className="relative translate-y-7 opacity-0"
+								id="link">
+								<Link
+									className="text-white text-[48px] font-normal tracking-tighter leading-tight"
+									href={item.href}>
+									{item.title}
+								</Link>
+							</div>
+						))}
 					</div>
-					<div className="links">
-						<div className="link">
-							<Link href="/">Projects</Link>
-						</div>
-						<div className="link">
-							<Link href="/">Expertise</Link>
-						</div>
-						<div className="link">
-							<Link href="/">Agency</Link>
-						</div>
-						<div className="link">
-							<Link href="/">Contact</Link>
-						</div>
-					</div>
-					<div className="video-wrapper">
+					<div
+						className="video-wrapper w-full h-[400px] bg-[#1a1a1a] overflow-hidden p-8 rounded-[10px]"
+						style={{
+							clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
+						}}>
 						<video
+							className="w-full h-full object-cover"
 							muted
 							src="/video.mp4"
 							loop
@@ -187,30 +164,53 @@ export default function Menu() {
 						/>
 					</div>
 				</div>
-				<div className="col col-2">
-					<div className="socials">
-						<div className="sub-col">
-							<p>Avaro</p>
-							<p>9 quao Androe Rockfield</p>
-							<p>69001 Ontario</p>
-							<p>Canada</p>
-							<br />
-							<p>contact@Avaro.fr</p>
-							<p>job@Avaro.fr</p>
-						</div>
-						<div className="sub-col">
-							<p>Instagram</p>
-							<p>LinkedIn</p>
-							<p>Twitter</p>
-							<p>Facebook</p>
-							<br />
-							<p>01 62 31 82 42</p>
-						</div>
+				<div className="relative h-full flex flex-col items-end justify-between pt-32 px-8 pb-8 flex-1 sm:hidden xm:hidden">
+					<div className="flex w-1/2 gap-20">
+						{socialItems.map((item) => (
+							<div
+								key={item.id}
+								id="socials">
+								<div className="flex flex-col gap-1">
+									{item.links1?.map((link) => (
+										<Link
+											id="link"
+											key={link.id}
+											className="text-sm text-white uppercase translate-y-7 opacity-0 relative font-medium"
+											href={link.href}>
+											{link.title}
+										</Link>
+									))}
+								</div>
+								<div className="flex flex-col gap-1">
+									{item.links2?.map((link) => (
+										<Link
+											id="link"
+											key={link.id}
+											className="text-sm text-white uppercase translate-y-7 opacity-0 relative font-medium"
+											href={link.href}>
+											{link.title}
+										</Link>
+									))}
+								</div>
+							</div>
+						))}
 					</div>
-
-					<div className="header">
-						<h1>Avaro</h1>
-					</div>
+					<motion.div className="overflow-hidden flex">
+						{"avaro".split("").map((item: string, i: number) => (
+							<motion.p
+								className="text-[300px] text-white uppercase font-normal leading-tight tracking-tighter"
+								initial={{ y: "100%" }}
+								whileInView={menuOpen ? { y: 0 } : { y: "100%" }}
+								transition={{
+									delay: i * 0.08,
+									duration: 1.2,
+									ease: [0.4, 0, 0.2, 1],
+								}}
+								key={i}>
+								{item}
+							</motion.p>
+						))}
+					</motion.div>
 				</div>
 			</div>
 		</div>
---
+++ constants/index.ts
@@ -0,0 +1,90 @@
+export const navItems = [
+   {
+      id: 1,
+      title: "Projects",
+      href: "/",
+   },
+   {
+      id: 2,
+      title: "Expertise",
+      href: "/",
+   },
+   {
+      id: 3,
+      title: "Agency",
+      href: "/",
+   },
+   {
+      id: 4,
+      title: "Contact",
+      href: "/",
+   },
+];
+
+export const socialItems = [
+   {
+      id: 1,
+      links1: [
+         {
+            id: 1,
+            title: "Avaro",
+            href: "/",
+         },
+         {
+            id: 2,
+            title: "9 quao Androe Rockfield",
+            href: "/",
+         },
+         {
+            id: 3,
+            title: "69001 Ontario",
+            href: "/",
+         },
+         {
+            id: 4,
+            title: "Canada",
+            href: "/",
+         },
+         {
+            id: 5,
+            title: "contact@Avaro.fr",
+            href: "/",
+         },
+         {
+            id: 6,
+            title: "job@Avaro.fr",
+            href: "/",
+         },
+      ]
+   },
+   {
+      id: 2,
+      links2: [
+         {
+            id: 7,
+            title: "Instagram",
+            href: "/",
+         },
+         {
+            id: 8,
+            title: "LinkedIn",
+            href: "/",
+         },
+         {
+            id: 9,
+            title: "Twitter",
+            href: "/",
+         },
+         {
+            id: 10,
+            title: "Facebook",
+            href: "/",
+         },
+         {
+            id: 11,
+            title: "01 62 31 82 42",
+            href: "/",
+         },
+      ]
+   },
+];
